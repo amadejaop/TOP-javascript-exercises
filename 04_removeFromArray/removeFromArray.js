06f4a12ae4e6762716baa8c45d8b8a8646684eb0
Amadeja <obsteter.amadeja@gmail.com>
2024-01-07 11:47:44 +0100

    Solve the first four exercises
    Managed to solve the first four exercises in the Odin Project
    javascript-exercises series. All of these exercises completely pass the
    provided tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+ for (const arg of args) {
+    if (arr.includes(arg)) {
+      // find the index of the arg in the arr
+      // remove the arg from the arr
+      let index = arr.indexOf(arg);
+      arr.splice(index, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
