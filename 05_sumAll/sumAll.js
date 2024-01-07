61127b5be8bee9898905f6b704b842e8f71ca595
Amadeja <obsteter.amadeja@gmail.com>
2024-01-07 12:41:02 +0100

    Solve exercises 5 and 6
    Solved exercises 5 and 6, both of these pass all of the provided tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+    return 'ERROR';
+  }
+  
+  if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  }
 
+  let larger = Math.max(num1, num2);
+  let smaller = Math.min(num1, num2);
+  let sum = 0;
+  for (let i = smaller; i <= larger; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
