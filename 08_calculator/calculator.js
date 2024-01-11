e82a28d598f20632286a7cc40576dd7c19127950
Amadeja <obsteter.amadeja@gmail.com>
2024-01-11 17:27:27 +0100

    Solve exercise 8 and 9
    Solved exercises 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,66 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let arrLength = arr.length;
+  let sum = 0;
+  if (arrLength === 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < arrLength; i++) {
+      sum += arr[i];
+  }
+    return sum;
+  }
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let arrLength = arr.length;
+  let result = 0;
 
+  if (arrLength === 0) {
+    return 0;
+  } else if (arrLength === 1) {
+    return arr[0];
+  } else {
+    result = arr[0] * arr[1];
+    if (arrLength > 2) {
+      for (let i = 2; i < arrLength; i++) {
+        result *= arr[i];
+      }
+    }
+    return result;
+  }
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let result = 0;
+  if (base === 0 && exponent === 0) {
+    return 1;
+  } else if (exponent === 1) {
+    return base;
+  } else {
+    result = base * base;
+    if (exponent > 2) {
+      for (let i = 2; i < exponent; i++) {
+        result *= base;
+      }
+    }
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+ let cleanedString = string.replace(/[^a-z0-9]/gi, '');
+ const strLen = cleanedString.length;
+ cleanedString = cleanedString.toLowerCase();
+ const half = Math.floor(strLen / 2);
+ let isPalindrome = true;
 
+ for (let i = 0; i < half; i++) {
+   isPalindrome = (cleanedString[i] === cleanedString[strLen - 1 - i]);
+   if (!isPalindrome) {
+    return false;
+   }
+ }
+return true;
 };
 
 // Do not edit below this line
