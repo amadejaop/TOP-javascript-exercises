cebb58fe6907e01a3bc38923420f73f02a5ab104
Amadeja <obsteter.amadeja@gmail.com>
2024-01-15 06:36:40 +0100

    Solve 10th exercise
    Solved the fibonacci exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  num = parseInt(num);
+  if (num < 0) {
+    return 'OOPS';
+  } else if (num === 0) {
+    return 0;
+  } else if (num === 1 || num === 2) {
+    return 1;
+  }
 
+  let fibonacci = [1, 1];
+
+  for (let i = 2; i < num; i++) {
+    fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+  }
+  return fibonacci[num-1];
 };
 
 // Do not edit below this line
