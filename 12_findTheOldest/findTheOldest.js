4bd4e5644669c09fd4409f137bd317685e921d24
Amadeja <obsteter.amadeja@gmail.com>
2024-01-17 19:52:00 +0100

    Solved exercises 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let currentYear = (new Date()).getFullYear();
+    let oldestAge = 0;
+    let index = 0;
+    let numPeople = arr.length;
 
+    for (let i = 0; i < numPeople; i++) {
+        if (arr[i].hasOwnProperty('yearOfDeath')) {
+            let result = arr[i].yearOfDeath - arr[i].yearOfBirth;
+            if (result > oldestAge) {
+                oldestAge = result;
+                index = i;
+            }
+        } else {
+            let result = currentYear - arr[i].yearOfBirth;
+            if (result > oldestAge) {
+                oldestAge = result;
+                index = i;
+            }
+        }
+    }
+    console.log(arr[index].name);
+    return arr[index];
 };
 
 // Do not edit below this line
